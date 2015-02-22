ad84785da91d2b513c4aef302742b70ba417983d
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-02-21 18:05:22 -0700

    Initial Commit

---
+++ CS 3240 Lexer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 3240 Lexer
+//
+//  Created by Dustin Hurst on 2/21/15.
+//  Copyright (c) 2015 Dustin Hurst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
